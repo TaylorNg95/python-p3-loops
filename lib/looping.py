cf267c4e606a7d93e0730176300dc3f12483977c
Taylor Ng <taylormng95@gmail.com>
2024-06-21 16:14:30 -0400

    completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [item * item for item in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 != 0:
+            print('Fizz')
+        elif i % 3 != 0 and i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print(i)
+        i += 1
